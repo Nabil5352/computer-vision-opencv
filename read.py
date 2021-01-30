88c0838442405528d5eb0e46544845b924db28d5
nabil5352 <nabil.ibn.mahmud@gmail.com>
2021-01-30 17:32:51 +0600

    image/video read

---
+++ read.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+# img = cv.imread('./images/1-colorful-painting-by-francoise-nielly.jpg')
+
+# cv.imshow('Painting', img)
+
+# cv.waitKey(0)
+
+capture = cv.VideoCapture('videos/tmux.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Playing', frame)
+
+    if cv.waitKey(20) & 0xff==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
